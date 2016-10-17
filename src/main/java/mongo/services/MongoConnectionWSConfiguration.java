3ea02fd5b403af229936b8ca0e3dd306583097ee
Jacob Headlee <jacob.headlee@concordusa.com>
2016-10-17 12:03:31 -0500

    Initial commit

---
+++ src/main/java/mongo/services/MongoConnectionWSConfiguration.java
@@ -0,0 +1,13 @@
+package mongo.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoConnectionWSConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoConnectionWSConfiguration.class, args);
+	}
+
+}
