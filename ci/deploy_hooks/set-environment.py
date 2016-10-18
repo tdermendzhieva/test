da0b45a972a163565ee4f0aba36b5fb1c66fd1f0
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-18 17:08:47 -0500

    updating deployment group name

--- ci/deploy_hooks/set-environment.py
+++ ci/deploy_hooks/set-environment.py
@@ -8,6 +8,6 @@ fo = open('/var/allie-data/config/application.properties','rw+')
 print "file name ", fo.name
 
 fo.seek(0,2)
-line = fo.write(os.environ['DEPLOYMENT_GROUP_NAME'])
+line = fo.write("spring.profiles.active=" + os.environ['DEPLOYMENT_GROUP_NAME'])
 
 fo.close()
