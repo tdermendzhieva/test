79e03a15bb73bf3684cd7e08bce4180ddc628aaa
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-18 16:44:55 -0500

    updating script

--- ci/deploy_hooks/set-environment.py
+++ ci/deploy_hooks/set-environment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
+from __future__ import print_function
 import os
 
-from __future__ import print_function
 print(os.environ['DEPLOYMENT_GROUP_NAME'], file="/var/allie-data/config/application.properties")
 
 
