cf8fc3f50df3f2f21ab8022deab9660084521748
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-19 16:21:42 -0500

    auto stopping service

--- ci/deploy_hooks/stop-service.sh
+++ ci/deploy_hooks/stop-service.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if sudo service --status-all | grep -Fq 'allie-data' ; then
-    echo test
-    sudo service allie-data stop
-fi
+sudo service allie-data stop
+
 
 #delete jar and prop file
 sudo rm /var/allie-data/*.jar
