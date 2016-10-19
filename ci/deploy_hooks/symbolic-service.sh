c2fa07ab18920876c57d9e41ea807413b141baaf
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-19 16:30:28 -0500

    adding script to restart service

--- ci/deploy_hooks/symbolic-service.sh
+++ ci/deploy_hooks/symbolic-service.sh
@@ -9,3 +9,4 @@ sudo ln -s -f /var/allie-data/*.jar /etc/init.d/allie-data
 sudo service allie-data start
 #then register it as a long running service
 sudo chkconfig allie-data on
+sudo service allie-data restart
