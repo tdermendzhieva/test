387a49bc376e0785c5ee37427c44a145441020e0
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-19 15:14:32 -0500

    updating remove script

--- ci/deploy_hooks/stop-service.sh
+++ ci/deploy_hooks/stop-service.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo service allie-data stop
+if sudo service --status-all | grep -Fq 'allie-data' ; then
+    echo test
+    sudo service allie-data stop
+fi
+
+#delete jar
+sudo rm /var/allie-data/*.jar
+#delete symbolic link
+sudo rm /etc/init.d/allie-data
