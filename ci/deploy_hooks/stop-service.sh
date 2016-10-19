4322c4b846916fd27998aa3fd4e3d16f27d3ee5e
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-19 15:34:17 -0500

    cleaning up prop file

--- ci/deploy_hooks/stop-service.sh
+++ ci/deploy_hooks/stop-service.sh
@@ -5,7 +5,8 @@ if sudo service --status-all | grep -Fq 'allie-data' ; then
     sudo service allie-data stop
 fi
 
-#delete jar
+#delete jar and prop file
 sudo rm /var/allie-data/*.jar
+sudo rm /var/allie-data/config/application.properties
 #delete symbolic link
 sudo rm /etc/init.d/allie-data
