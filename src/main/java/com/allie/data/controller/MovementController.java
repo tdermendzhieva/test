f9bdbcaa55608881bc03aa66730d04a24d4cf492
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-20 14:46:31 -0500

    movment swagger

--- src/main/java/com/allie/data/controller/MovementController.java
+++ src/main/java/com/allie/data/controller/MovementController.java
@@ -1,5 +1,9 @@
 package com.allie.data.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,10 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value="allie-data/v1")
+@Api(value = "locations", description = "Endpoint to store user's phone movement")
+
 public class MovementController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @ApiOperation(value = "Persistence service call to store a list of an allie user's phone movement.",
+            notes = "The service will asynchronously store all data in backend persistence structure.  If the request successfully reaches the service, a 202 (accepted) HttpStatus will " +
+                    "be returned")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "The service successfully received the request"),
+            @ApiResponse(code = 400, message = "The request was malformed to the point that no information can be stored"),
+            @ApiResponse(code = 500, message = "There was an unspecified server error.")
+    })
     @RequestMapping(value="/movements", method= RequestMethod.POST, consumes = "application/json")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void postMovementTelemetry() {
