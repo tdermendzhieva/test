878ff662b94ba25d47227701f461bbb44f8844d0
jacobheadlee <jacob.headlee@concordusa.com>
2016-10-25 13:11:10 -0500

    adding stub get user event

--- src/main/java/com/allie/data/controller/UserEventController.java
+++ src/main/java/com/allie/data/controller/UserEventController.java
@@ -8,6 +8,8 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 /**
  * Created by andrew.larsen on 10/24/2016.
  */
@@ -33,4 +35,38 @@ public class UserEventController {
                                           @RequestHeader(name = "x-allie-correlation-id") String correlationId) {
       //  service.insertEvent(request);
     }
+
+    @ApiOperation(value = "Persistence service call to retrieve Allie User Events",
+            notes = "The service will get all Allie User Events for a given user on a given day (defaults to today)")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The events were successfully retrieved")
+    })
+    @RequestMapping(value = "user/{allieId}/events", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserEventDTO> getUserEvent(@RequestParam String allieId,
+                                           @RequestParam(name = "received_date", defaultValue = "") String recievedDate,
+                                           @RequestHeader(name = "x-allie-request-id") String requestId,
+                                           @RequestHeader(name = "x-allie-correlation-id") String correlationId
+    ) {
+
+        List<UserEventDTO> mockList = new ArrayList<>();
+        UserEventDTO mock = new UserEventDTO();
+        mock.setAllieId("allieId");
+        mock.setEventReceivedTimestamp("2010-10-10T10:10:10.101Z");
+        Map neuraJson = new HashMap();
+        neuraJson.put("identifier", "YourEventIdentifier_userIsOnTheWayHome");
+        neuraJson.put("userId", "allieId");
+        Map event = new HashMap();
+        event.put("name", "userIsOnTheWayHome");
+        event.put("timestamp", 1477342584);
+        Map metaData = new HashMap();
+        event.put("metadata", metaData);
+        neuraJson.put("event", event);
+        mock.setNeuraJson(neuraJson);
+        mockList.add(mock);
+        return mockList;
+
+        //return service.selectUserEvent(allieId, recievedDate);
+    }
+
 }
