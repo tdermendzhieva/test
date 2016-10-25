1e3cec1f790649f6e59a90d3a036374302e43529
Andrew Larsen <andrew.larsen@concordusa.com>
2016-10-25 09:15:03 -0500

    Update UsersController.java
    removed extra import

--- src/main/java/com/allie/data/controller/UsersController.java
+++ src/main/java/com/allie/data/controller/UsersController.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URLDecoder;
-
 /**
  * Created by jacob.headlee on 10/20/2016.
  */
