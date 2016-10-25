36641ccc2679369dd1705fbb85348324bb79350f
andrewlarsen514 <andrew.larse514@gmail.com>
2016-10-25 11:35:07 -0500

    adding event mdeol

---
+++ src/main/java/com/allie/data/jpa/model/UserEvent.java
@@ -0,0 +1,51 @@
+package com.allie.data.jpa.model;
+
+import com.mongodb.DBObject;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by andrew.larsen on 10/24/2016.
+ */
+@Document(collection="RawNeuraEvents")
+public class UserEvent{
+
+    @Id
+    private String id;
+    private String allieId;
+    private String eventReceivedTimestamp;
+    private DBObject neuraJson;
+
+    public String getAllieId() {
+        return allieId;
+    }
+
+    public void setAllieId(String allieId) {
+        this.allieId = allieId;
+    }
+
+    public String getEventReceivedTimestamp() {
+        return eventReceivedTimestamp;
+    }
+
+    public void setEventReceivedTimestamp(String eventReceivedTimestamp) {
+        this.eventReceivedTimestamp = eventReceivedTimestamp;
+    }
+
+    public DBObject getNeuraJson() {
+        return neuraJson;
+    }
+
+    public void setNeuraJson(DBObject neuraJson) {
+        this.neuraJson = neuraJson;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
