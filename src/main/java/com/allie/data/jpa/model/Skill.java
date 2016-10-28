20d56263614ad5526204cee3ac53a1cb4c2a76e7
andrewlarsen514 <andrew.larse514@gmail.com>
2016-10-28 14:12:29 -0500

    adding models

--- src/main/java/com/allie/data/jpa/model/Skill.java
+++ src/main/java/com/allie/data/jpa/model/Skill.java
@@ -4,4 +4,30 @@ package com.allie.data.jpa.model;
  * Created by andrew.larsen on 10/28/2016.
  */
 public class Skill {
+    private String skillId;
+    private String description;
+
+    public String getSkillId() {
+        return skillId;
+    }
+
+    public void setSkillId(String skillId) {
+        this.skillId = skillId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Skill(String skillId, String description) {
+        this.skillId = skillId;
+        this.description = description;
+    }
+
+    public Skill() {
+    }
 }
