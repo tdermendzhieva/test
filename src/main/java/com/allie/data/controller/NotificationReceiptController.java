5372a061ef3c0bb1b7a5ec0ba6e4a98a3fe55297
jacobheadlee <jacob.headlee@concordusa.com>
2016-11-01 14:45:37 -0500

    logging, comments, and swagger

--- src/main/java/com/allie/data/controller/NotificationReceiptController.java
+++ src/main/java/com/allie/data/controller/NotificationReceiptController.java
@@ -3,6 +3,9 @@ package com.allie.data.controller;
 import com.allie.data.dto.NotificationReceiptDTO;
 import com.allie.data.service.NotificationReceiptService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,15 @@ public class NotificationReceiptController {
     @Autowired
     NotificationReceiptService service;
 
+    @ApiOperation(value = "Persistence service call to store a notification receipt",
+        notes = "The service will asynchronously store all data in backend persistence structure.  If the request successfully reaches the service, a 202 (accepted) HttpStatus will " +
+                "be returned")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "The service successfully received the request"),
+            @ApiResponse(code = 400, message = "The request was malformed to the point that no information can be stored"),
+            @ApiResponse(code = 500, message = "There was an unspecified server error."),
+            @ApiResponse(code = 503, message = "There was an issue connecting to a downstream system")
+    })
     @RequestMapping(value="/notificationReceipts", method = RequestMethod.POST, consumes = "application/json")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void postNotificationReceipts(@RequestBody NotificationReceiptDTO notificationReceipt,
--- src/main/java/com/allie/data/service/NotificationReceiptService.java
+++ src/main/java/com/allie/data/service/NotificationReceiptService.java
@@ -4,6 +4,8 @@ import com.allie.data.dto.NotificationReceiptDTO;
 import com.allie.data.factory.NotificationReceiptFactory;
 import com.allie.data.jpa.model.NotificationReceipt;
 import com.allie.data.repository.NotificationReceiptRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -12,6 +14,9 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 @Component
 public class NotificationReceiptService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
     NotificationReceiptRepository repository;
     NotificationReceiptFactory factory;
 
@@ -21,10 +26,13 @@ public class NotificationReceiptService {
     }
 
     public NotificationReceipt insertNotificationReceipt(NotificationReceiptDTO notificationReceiptDTO) {
+        //get a jpa notification receipt from the dto
         NotificationReceipt notificationReceipt = factory.createNotificationReceipt(notificationReceiptDTO);
+        //make sure we have an allie Id
         if(notificationReceipt.getAllieId() != null && !notificationReceipt.getAllieId().trim().isEmpty()) {
             return repository.insert(notificationReceipt);
         } else {
+            logger.debug("request was missing required allieId: " + notificationReceiptDTO);
             throw new IllegalArgumentException("missing required allieId");
         }
     }
